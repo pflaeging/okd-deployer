26303e3617aaf24af5933e1f0e2ad14a3464021a
Novomatic Admin <office@pflaeging.net>
2019-10-10 12:42:44 +0200

    Add new root to all members

--- deploy-root-ca/deploy-root-ca.sh
+++ deploy-root-ca/deploy-root-ca.sh
@@ -5,7 +5,7 @@
 for member in $CLUSTERMEMBERS
 do
     echo $member 
-    echo ssh member update-ca-trust force-enable
-    echo scp NGIRootCA01.crt $member:/etc/pki/ca-trust/source/anchors/
-    echo ssh member update-ca-trust extract
+    ssh $member update-ca-trust force-enable
+    scp NGIRootCA01.crt $member:/etc/pki/ca-trust/source/anchors/
+    ssh $member update-ca-trust extract
 done
