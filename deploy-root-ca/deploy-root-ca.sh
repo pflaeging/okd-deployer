cf426a9162bcd1e8feca6fb0c8226522cb0adea9
Peter Pfläging <peter@pflaeging.net>
2020-02-10 13:14:57 +0100

    rm customer2

--- deploy-root-ca/deploy-root-ca.sh
+++ deploy-root-ca/deploy-root-ca.sh
@@ -6,6 +6,6 @@ for member in $CLUSTERMEMBERS
 do
     echo $member 
     ssh $member update-ca-trust force-enable
-    scp NGIRootCA01.crt $member:/etc/pki/ca-trust/source/anchors/
+    scp  MyRouteCaComesHere.crt $member:/etc/pki/ca-trust/source/anchors/
     ssh $member update-ca-trust extract
 done
